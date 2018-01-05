6f0bc6916de663b1d108291740e0773350e95c4c
Skylark <li_xuelong@126.com>
2018-01-05 15:56:36 +0800

    初始化仓库

---
+++ app/src/main/java/online/himakeit/readhub/MainActivity.java
@@ -0,0 +1,20 @@
+package online.himakeit.readhub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author：LiXueLong
+ * @date:2018/1/5-15:31
+ * @mail1：skylarklxlong@outlook.com
+ * @mail2：li_xuelong@126.com
+ * @des：MainActivity
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
